db82666765cffef39b4cac3aff15d110bfabb133
jfederico <jesus@123it.ca>
2020-04-27 21:39:09 -0400

    percisted certbot logs and added env options to init-letsencrypt

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -11,8 +11,8 @@ echo $URL_HOST
 domains=($URL_HOST)
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="$LETSENCRYPT_EMAIL" # Adding a valid address is strongly recommended
+staging=${LETSENCRYPT_STAGING:-0} # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
